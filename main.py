da0c876703b0a91dc4c2c098968438e5ad4ae19e
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-10-27 10:49:35 -0300

    tentando POO

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+class Sistema():
+    def __init__(self,dicio_itens):
+        self.dicio_itens=dicio_itens
+    def exibir_itens(self):
+        print(self.dicio_itens)
+    def procurar_itens(self,item):
+        if item in self.dicio_itens.keys():
+           print(f"O item {item} está em estoque, com {self.dicio_itens[item]}")
+        else:
+            print(f"O item {item} não está em estoque.")
+    def atualizar_itens(self,item):
+        if item in self.dicio_itens.keys():
+            
+            
+        
