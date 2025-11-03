b8f8a513f361c7ab36877c8f8e32f56667a1d836
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-02 21:08:00 -0300

    colocando aleatoriedades no código

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import json
+class Cores:
+    verde='\033[92m'
+    vermelho='\033[91m'
+    amarelo='\033[93m'
+    azul='\033[94m'
+    reset='\033[0m'
 class SistemaUm():  #Aqui é a lógica do sistema em si
     """Sisteminha CRUD completinho 
     Todos os produtos devem estar em uma lista de dicionários,
@@ -16,7 +22,7 @@ class SistemaUm():  #Aqui é a lógica do sistema em si
                 for produto in self.lista_dict_produtos:
                     self.set_codigos.add(produto["Código"])
         except FileNotFoundError:
-            print("JSON de nome 'estoque.json' não encontrado!")
+            print(f"{Cores.vermelho}JSON de nome 'estoque.json' não encontrado!{Cores.reset}")
             return
     def salvar_dados(self): #Também usa JSON
         with open("estoque.json","w") as arquivo:
@@ -27,7 +33,7 @@ class SistemaUm():  #Aqui é a lógica do sistema em si
             for x in encontrados:
                 print(json.dumps(x, ensure_ascii=False, indent=2))
         else:
-            print("Não encontrado! Por favor, cheque novamente ou adicione!")
+            print(f"{Cores.vermelho}Não encontrado! Por favor, cheque novamente ou adicione!{Cores.reset}")
         return 
     def listar_por_categoria(self,categoria):
         lista_categoria_temp=[p for p in self.lista_dict_produtos if p["Categoria"].lower()== categoria.lower()]
@@ -46,19 +52,19 @@ class SistemaUm():  #Aqui é a lógica do sistema em si
             print("Nenhum encontrado.")
     def cadastrar_produto(self,nome,codigo,categoria,preco,quantidade): #Cria e add um produto novo
         if codigo in self.set_codigos:
-            print("Código já existente, por favor, tente novamente!")
+            print(f"{Cores.vermelho}Código já existente, por favor, tente novamente!{Cores.reset}")
             return     
         if categoria not in self.tupla_categorias:
-            print(f"Categoria inexistente! Categorias possíveis: {self.tupla_categorias}")
+            print(f"{Cores.vermelho}Categoria inexistente! Categorias possíveis: {self.tupla_categorias}{Cores.reset}")
             return
         if len(nome)<1:
-            print("Nome inválido, por favor tente novamente!")
+            print(f"{Cores.vermelho}Nome inválido, por favor tente novamente!{Cores.reset}")
             return
         if preco<0:
-            print("Preço inválido, por favor, tente novamente!")
+            print(f"{Cores.vermelho}Preço inválido, por favor, tente novamente!{Cores.reset}")
             return
         if quantidade<0:
-            print("Quantidade inválida, por favor tente novamente!")
+            print(f"{Cores.vermelho}Quantidade inválida, por favor tente novamente!{Cores.reset}")
             return
         produto={
             "Código":codigo,
@@ -70,7 +76,7 @@ class SistemaUm():  #Aqui é a lógica do sistema em si
         self.lista_dict_produtos.append(produto)
         self.set_codigos.add(codigo)
         self.salvar_dados()
-        print(f"Produto '{nome}' cadastrado com sucesso!")
+        print(f"{Cores.verde}Produto '{nome}' cadastrado com sucesso!{Cores.reset}")
     
     def listar_produtos(self): #Mostra todos os produtos em estoque
         for p in self.lista_dict_produtos:
@@ -78,53 +84,53 @@ class SistemaUm():  #Aqui é a lógica do sistema em si
     
     def atualizar_produto(self,codigo,campo,novo_valor):
         if codigo not in self.set_codigos or campo.lower() not in ["nome","categoria","preço","estoque","codigo"]:
-            print("Parâmetros inválidos!")
+            print(f"{Cores.vermelho}Parâmetros inválidos!{Cores.reset}")
             return
         if type(novo_valor)==str and len(novo_valor)<1:
-            print("Por favor, insira um novo valor válido!")
+            print(f"{Cores.vermelho}Por favor, insira um novo valor válido!{Cores.reset}")
             return
         if (type(novo_valor)==float or type(novo_valor)==int) and novo_valor<0:
-            print("Por favor, insira um valor numérico válido!")
+            print(f"{Cores.vermelho}Por favor, insira um valor numérico válido!{Cores.reset}")
             return
         for i in range(len(self.lista_dict_produtos)):
             if self.lista_dict_produtos[i]["Código"]==codigo:
                 if campo.lower()=="nome":
                     self.lista_dict_produtos[i]["Nome"]=novo_valor
-                    print("Nome atualizado com sucesso!")
+                    print(f"{Cores.azul}Nome atualizado com sucesso!{Cores.reset}")
                 if campo.lower()=="categoria":
                     self.lista_dict_produtos[i]["Categoria"]=novo_valor
-                    print("Categoria atualizada com sucesso!")
+                    print(f"{Cores.azul}Categoria atualizada com sucesso!{Cores.reset}")
                 if campo.lower()=="preço":
                     self.lista_dict_produtos[i]["Preço"]=float(novo_valor)
-                    print("Preço atualizado com sucesso!")
+                    print(f"{Cores.azul}Preço atualizado com sucesso!{Cores.reset}")
                 if campo.lower()=="estoque":
                     self.lista_dict_produtos[i]["Estoque"]=float(novo_valor)
-                    print("Estoque atualizado com sucesso!")
+                    print(f"{Cores.azul}Estoque atualizado com sucesso!{Cores.reset}")
                 if campo.lower()=="codigo":
                     self.set_codigos.remove(codigo)
                     self.lista_dict_produtos[i]["Código"]=int(novo_valor)
                     self.set_codigos.add(novo_valor)
-                    print("Código atualizado com sucesso!")
+                    print(f"{Cores.azul}Código atualizado com sucesso!{Cores.reset}")
         self.salvar_dados()
         return
             
     def buscar_produto(self,codigo): #Exibe informações de um produto específico
         if codigo not in self.set_codigos:
-            print("Código inválido!")
+            print(f"{Cores.vermelho}Código inválido!{Cores.reset}")
             return
         for i in range(len(self.lista_dict_produtos)):
             if self.lista_dict_produtos[i]["Código"]==codigo:
                 print(str(self.lista_dict_produtos[i]).replace("'","").replace("{","").replace("}",""))
     def excluir_produto(self,codigo): #Exclui um produto específico
         if codigo not in self.set_codigos:
-            print("Código inválido!")
+            print(f"{Cores.vermelho}Código inválido!{Cores.reset}")
             return
         for i in range(len(self.lista_dict_produtos)):
             if self.lista_dict_produtos[i]["Código"]==codigo:
                 self.lista_dict_produtos.pop(i)
                 self.set_codigos.remove(codigo)
                 self.salvar_dados()
-                print("Produto removido com sucesso!")
+                print(f"{Cores.azul}Produto removido com sucesso!{Cores.reset}")
                 break
     def sair_sistema(self): #Encerra o programa (o mais difícil de se programar kkkkkkkk)
         print("Saindo...")
@@ -194,7 +200,7 @@ while True:
                     try:
                         codigo=int(input("Qual o código do produto? (apenas números.)"))
                     except ValueError:
-                            print("Por favor, insira apenas números!")
+                            print(f"{Cores.vermelho}Por favor, insira apenas números!{Cores.reset}")
                             continue
                     sistema_um.excluir_produto(codigo)
                 else:
