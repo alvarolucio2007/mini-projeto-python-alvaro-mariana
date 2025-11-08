bb132a6dcce318c1af7c7132ee8cafe70549a80b
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-07 23:08:00 -0300

    CABEI FRONT BORA VASCOOOOOO

--- questao_1/questao_1.py
+++ questao_1/questao_1.py
@@ -1,22 +1,20 @@
 import json
+
 import typing
 import streamlit as st
 import pandas as pd
+import os
 
-class Cores:
-    VERDE :str ='\033[92m' #streamlit 
-    VERMELHO :str ='\033[91m'
-    AMARELO :str ='\033[93m'
-    AZUL :str ='\033[94m'
-    RESET:str='\033[0m'
 ProdutoDict=dict[str,str|float|int]
 ListaEstoque=list[ProdutoDict]
 class ControleEstoque:  #Aqui é a lógica do sistema em si
+    
     """Sisteminha CRUD completinho 
     Todos os produtos devem estar em uma lista de dicionários,
         que possuem códigos, guardados em um set, e ter uma tupla com categorias"""
     lista_dict_produtos:ListaEstoque
     set_codigos:set[int]
+    JSON_PATH=os.path.join(os.getcwd(),"estoque.json")
     def __init__(self):
         self.lista_dict_produtos =[] #Cria uma lista vazia, toda vez que rodar, vai estar vazia
         self.set_codigos=set() #Cria um set vazio, ja que 2 produtos não possuem o mesmo código
@@ -24,147 +22,125 @@ class ControleEstoque:  #Aqui é a lógica do sistema em si
         self.carregar_dados()
     
     def carregar_dados(self) -> None: #Usa JSON
+        if not os.path.exists(self.JSON_PATH):
+            with open(self.JSON_PATH,"w") as f:
+                json.dump([],f)
+            self.lista_dict_produtos=[]
         try:
-            with open("/home/alvaro/Códigos/mini-projeto/mini-projeto-python-alvaro-mariana/questao_1/estoque.json","r") as arquivo:
-                dados_json : ListaEstoque =json.load(arquivo)
-                self.lista_dict_produtos=dados_json
-                for produto in self.lista_dict_produtos:
-                    self.set_codigos.add(int(produto["Código"]))
+            with open(self.JSON_PATH,"r") as arquivo:
+                try:
+                    dados_json:ListaEstoque=json.load(arquivo)
+                    self.lista_dict_produtos=dados_json
+                    for produto in self.lista_dict_produtos:
+                        produto["Código"]=int(produto["Código"])
+                        produto["Preço"]=float(produto["Preço"])
+                        produto["Estoque"]=float(produto["Estoque"])
+                        self.set_codigos.add(int(produto["Código"]))
+                except json.JSONDecodeError:
+                    self.lista_dict_produtos=[]
+                    self.salvar_dados()
+                    pass
         except FileNotFoundError:
-            print(f"{Cores.VERMELHO}JSON de nome 'estoque.json' não encontrado!{Cores.RESET}")
-            return
+            with open(self.JSON_PATH,"w") as f:
+                json.dump([],f)
+            self.lista_dict_produtos=[]
+            return False
+        return
     
     def salvar_dados(self) -> None: #Também usa JSON
-        with open("/home/alvaro/Códigos/mini-projeto/mini-projeto-python-alvaro-mariana/questao_1/estoque.json","w") as arquivo:
+        with open(self.JSON_PATH,"w") as arquivo:
             json.dump(self.lista_dict_produtos,arquivo)
    
-    def buscar_nome(self,nome : str) -> list|None:
+    def buscar_nome(self,nome : str) -> list:
         encontrados=[p for p in self.lista_dict_produtos if nome.lower() in str(p["Nome"]).lower()]
         if encontrados:
             return encontrados
         else:
-            print(f"{Cores.VERMELHO}Não encontrado! Por favor, cheque novamente ou adicione!{Cores.RESET}")
-        return 
+            raise ValueError("Nenhum produto encontrado!")
+        return encontrados
    
-    def listar_por_categoria(self,categoria : str) -> list|None:
-        
+    def listar_por_categoria(self,categoria : str) -> list:
         lista_categoria_temp=[p for p in self.lista_dict_produtos if str(p["Categoria"]).lower()== categoria.lower()]
-        if lista_categoria_temp:
-            return lista_categoria_temp
-        else:
-            print(f"Nenhum produto encontrado na categoria '{categoria}'!")
-        return
+        return lista_categoria_temp
    
     def mostrar_estoque_baixo(self,limite : float) -> list:
         encontrado=[p for p in self.lista_dict_produtos if int(p["Estoque"])<limite]
-        if encontrado:
-            for p in encontrado:
-                return encontrado
-        else:
-            print("Nenhum encontrado.")
+        return encontrado
    
     def cadastrar_produto(self,nome:str,codigo:int,categoria:str,preco:float,quantidade:float) -> None :#Cria e add um produto novo
         if codigo in self.set_codigos:
-            print(f"{Cores.VERMELHO}Código já existente, por favor, tente novamente!{Cores.RESET}")
-            return     
+            raise ValueError("Código já existente!")
         if categoria not in self.tupla_categorias:
-            print(f"{Cores.VERMELHO}Categoria inexistente! Categorias possíveis: {self.tupla_categorias}{Cores.RESET}")
-            return
+            raise ValueError(f"Categoria inexistente! Categorias possíveis: {self.tupla_categorias}")
         if len(nome)<1:
-            print(f"{Cores.VERMELHO}Nome inválido, por favor tente novamente!{Cores.RESET}")
-            return
+            raise ValueError("Nome não pode estar vazio!")
         if preco<0:
-            print(f"{Cores.VERMELHO}Preço inválido, por favor, tente novamente!{Cores.RESET}")
-            return
+            raise ValueError("Preço não pode ser negativo!")
         if quantidade<0:
-            print(f"{Cores.VERMELHO}Quantidade inválida, por favor tente novamente!{Cores.RESET}")
-            return
+            raise ValueError("Quantidade não pode ser negativa!")
         produto : dict[str, typing.Any]={
-            "Código":codigo,
-            "Nome":nome,
-            "Categoria":categoria,
-            "Preço":preco,
-            "Estoque":quantidade #
+            "Código":int(codigo),
+            "Nome":str(nome),
+            "Categoria":str(categoria),
+            "Preço":float(preco),
+            "Estoque":float(quantidade) #
         }
         self.lista_dict_produtos.append(produto)
         self.set_codigos.add(codigo)
         self.salvar_dados()
-        print(f"{Cores.VERDE}Produto '{nome}' cadastrado com sucesso!{Cores.RESET}")
-    
-   
-    def listar_produtos(self) -> None: #Mostra todos os produtos em estoque
-        for p in self.lista_dict_produtos:
-            print(json.dumps(p,ensure_ascii=False,indent=2))
-    
+        
     def atualizar_produto(self,codigo:int,campo:str,novo_valor:str|int|float):
         if codigo not in self.set_codigos or campo.lower() not in ["nome","categoria","preço","estoque","codigo"]:
-            print(f"{Cores.VERMELHO}Parâmetros inválidos!{Cores.RESET}")
-            return
+            raise ValueError("Parâmetros inválidos!")
         if type(novo_valor) is str and len(novo_valor)<1:
-            print(f"{Cores.VERMELHO}Por favor, insira um novo valor válido!{Cores.RESET}")
-            return
+            raise ValueError("Por favor, insira um novo valor válido!")
         if (type(novo_valor) is float or type(novo_valor) is int) and novo_valor<0:
-            print(f"{Cores.VERMELHO}Por favor, insira um valor numérico válido!{Cores.RESET}")
-            return
+            raise ValueError("Por favor, insira um valor numérico válido!")
         for i in range(len(self.lista_dict_produtos)):
             if int(self.lista_dict_produtos[i]["Código"])==codigo:
                 if campo.lower()=="nome":
                     self.lista_dict_produtos[i]["Nome"]=str(novo_valor)
-                    print(f"{Cores.AZUL}Nome atualizado com sucesso!{Cores.RESET}")
-                if campo.lower()=="categoria":
+
+                elif campo.lower()=="categoria":
                     (self.lista_dict_produtos[i]["Categoria"])=str(novo_valor)
-                    print(f"{Cores.AZUL}Categoria atualizada com sucesso!{Cores.RESET}")
-                if campo.lower()=="preço":
-                    self.lista_dict_produtos[i]["Preço"]=(novo_valor)
-                    print(f"{Cores.AZUL}Preço atualizado com sucesso!{Cores.RESET}")
-                if campo.lower()=="estoque":
-                    self.lista_dict_produtos[i]["Estoque"]=(novo_valor)
-                    print(f"{Cores.AZUL}Estoque atualizado com sucesso!{Cores.RESET}")
-                if campo.lower()=="codigo":
+
+                elif campo.lower()=="preço":
+                    self.lista_dict_produtos[i]["Preço"]=float(novo_valor)
+
+                elif campo.lower()=="estoque":
+                    self.lista_dict_produtos[i]["Estoque"]=float(novo_valor)
+
+                elif campo.lower()=="codigo":
                     self.set_codigos.remove(codigo)
-                    self.lista_dict_produtos[i]["Código"]=(novo_valor)
+                    self.lista_dict_produtos[i]["Código"]=int(novo_valor)
                     self.set_codigos.add(int(novo_valor))
-                    print(f"{Cores.AZUL}Código atualizado com sucesso!{Cores.RESET}")
+                break
         self.salvar_dados()
         return
             
     def buscar_produto(self,codigo:int): #Exibe informações de um produto específico
         if codigo not in self.set_codigos:
-            print(f"{Cores.VERMELHO}Código inválido!{Cores.RESET}")
-            return
-        for i in range(len(self.lista_dict_produtos)):
-            if self.lista_dict_produtos[i]["Código"]==str(codigo):
-                print(str(self.lista_dict_produtos[i]).replace("'","").replace("{","").replace("}",""))
+            raise ValueError("Código não encontrado!")
+        for produto in self.lista_dict_produtos:
+            if int(produto["Código"])==codigo:
+                return produto
     
-    def excluir_produto(self,codigo:int): #Exclui um produto específico
+    def excluir_produto(self,codigo:int) -> None: #Exclui um produto específico
         if codigo not in self.set_codigos:
-            print(f"{Cores.VERMELHO}Código inválido!{Cores.RESET}")
-            return
+            raise ValueError(f"Código {codigo} não encontrado!")
         for i in range(len(self.lista_dict_produtos)):
-            if self.lista_dict_produtos[i]["Código"]==str(codigo):
+            if int(self.lista_dict_produtos[i]["Código"])==(codigo):
                 _=self.lista_dict_produtos.pop(i)
                 self.set_codigos.remove(codigo)
                 self.salvar_dados()
-                print(f"{Cores.AZUL}Produto removido com sucesso!{Cores.RESET}")
                 break
-    
-    def verificar_input(self,tipo:type[typing.Any] ,nome:str|float|int) -> typing.Any:
-        while True:
-            valor_str=input(f"Qual o valor para {nome} ?")
-            try:
-                valor=tipo(valor_str)
-                return valor
-            except ValueError:
-                print("Inválido, por favor tente novamente!")
-    
-    def sair_sistema(self): #Encerra o programa (o mais difícil de se programar kkkkkkkk)
-        print("Saindo...")
         return
 
 class FrontEnd():
     def __init__(self) -> None:
         self.estoque=ControleEstoque()
         self.produtos=self.estoque.lista_dict_produtos
+        
     def renderizar_menu_lateral(self):
         with st.sidebar:
             st.header("Selecione a ação")
@@ -179,7 +155,7 @@ class FrontEnd():
                 nome=st.text_input("Nome do Produto", max_chars=100)
             with col2:
                 codigo=st.number_input("Código (ID)", min_value=1, step=1)
-            caregoria=st.selectbox("Categoria", self.estoque.tupla_categorias)
+            categoria=st.selectbox("Categoria", self.estoque.tupla_categorias)
             col3,col4=st.columns(2)
             with col3:
                 preco=st.number_input("Preço (R$)", min_value=0.01, format="%.2f")
@@ -188,14 +164,17 @@ class FrontEnd():
             clique_salvar=st.form_submit_button("Cadastrar e Salvar", use_container_width=True)
             if clique_salvar:
                 try:
-                    self.estoque.atualizar_produto(
+                    self.estoque.cadastrar_produto(
+                        nome=nome,
                         codigo=int(codigo),
-                        campo=tipo_de_dado.lower(),
-                        novo_valor=novo_valor
+                        categoria=categoria,
+                        preco=float(preco),
+                        quantidade=float(quantidade)
                     )
-                    st.success(f"Produto {codigo} atualizado com sucesso! Novo {tipo_de_dado}: {novo_valor}")
+                    st.success(f"Produto {codigo} adicionado com sucesso (Código: {codigo})")
+                    st.rerun()
                 except Exception as e:
-                    st.error(f"Falha na atualização: {e}")
+                    st.error(f"Falha no cadastro! Verifique se o código já existe. Código de erro: {e}")
         return
     
     def renderizar_atualizar(self) -> None:
@@ -228,53 +207,99 @@ class FrontEnd():
                         novo_valor=novo_valor
                     )
                     st.success(f"Produto {codigo} atualizado com sucesso! Novo {tipo_de_dado}: {novo_valor}")
+                    st.rerun()
                 except Exception as e:
                     st.error(f"Falha na atualização: {e}")
         return 
     def renderizar_buscar(self) -> None:
-        st.markdown("Rendeirização de produto")
+        st.markdown("Busca de produto")
         tipo_busca=st.selectbox("Tipo de busca",("Nome","Categoria","Estoque Baixo"))
         if tipo_busca=="Nome":
             busca=st.text_input("Qual seria o nome?",max_chars=100)
-            try:
-                st.dataframe(
-                    self.estoque.buscar_nome(busca),
-                    use_container_width=True,
-                    hide_index=True
-                )
-            except Exception as e:
-                st.error(f"Falha na busca: {e}")
+            if busca:
+                resultados=self.estoque.buscar_nome(busca)
+                df_resultados=pd.DataFrame(resultados)
+                if not df_resultados.empty:
+                    st.success(f"Encontrado(s) {len(df_resultados)} produto(s).")
+                    st.dataframe(df_resultados,use_container_width=True,hide_index=True)
+                else:
+                    st.warning(f"Nenhum produto encontrado com o nome '{busca}'")
         elif tipo_busca=="Categoria":
             busca=st.selectbox("Categoria",self.estoque.tupla_categorias)
-            try:
-                st.dataframe(
-                    self.estoque.listar_por_categoria(busca),
-                    use_container_width=True,
-                    hide_index=True
-                )
-            except Exception as e:
-                st.error(f"Falha na busca: {e}")
+            resultados=self.estoque.listar_por_categoria(busca)
+            df_resultados=pd.DataFrame(resultados)
+            if not df_resultados.empty:
+                st.success(f"Encontado(s) {len(df_resultados)} produto(s).")
+                st.dataframe(df_resultados,use_container_width=True,hide_index=True)
         else:
             busca=st.number_input("Qual seria o limite?",min_value=0,step=1)
-            try:
-                st.dataframe(
-                    self.estoque.mostrar_estoque_baixo(busca),
-                    use_container_width=True,
-                    hide_index=True
-                )
-            except Exception as e:
-                st.error(f"Falha na busca: {e}")
+            resultados=self.estoque.mostrar_estoque_baixo(busca)
+            df_resultados=pd.DataFrame(resultados)
+            if not df_resultados.empty:
+                st.warning(f"{len(df_resultados)} produto(s) estão abaixo do limite!")
+                st.dataframe(df_resultados,use_container_width=True,hide_index=True)
+            else:
+                st.success("Todos os produtos estão acima do limite de estoque!")  
         return
+    def renderizar_excluir(self) -> None:
+        st.markdown("Excluir Produto")
+        st.warning("Esta ação é permanente e não pode ser desfeita.")
+        if 'codigo_excluir' not in st.session_state:
+            st.session_state.codigo_excluir=None
+        with st.form("form_exclusao_busca"):
+            codigo_excluir_input=st.number_input("Insira o código (ID) do produto a ser excluído:",min_value=1,step=1,key="codigo_excluir_input_widget")
+            
+            clique_buscar=st.form_submit_button("Buscar Produto para Exclusão",use_container_width=True)
+            if clique_buscar:
+                st.session_state.codigo_excluir=int(codigo_excluir_input)
+                st.rerun()
+        
+        codigo_excluir=st.session_state.codigo_excluir
+            
+        if codigo_excluir and codigo_excluir in self.estoque.set_codigos:
+            nome_produto=next(
+                    (p["Nome"] for p in self.estoque.lista_dict_produtos if int(p["Código"]) == int(codigo_excluir)),
+                    "Produto Desconhecido")
+            st.error(f"Você está prestes a excluir o produto: {nome_produto} (Código: {int(codigo_excluir)})")
+            clique_excluir=st.button("CONFIRMAR EXCLUSÃO",use_container_width=True)
+            if clique_excluir:
+                try:
+                    self.estoque.excluir_produto(int(codigo_excluir))
+                    st.success(f"Produto {nome_produto} excluido com sucesso! ")
+                    st.rerun()
+                except Exception as e:
+                    st.error(f"Falha na exclusão. Erro:{e}")
+        elif codigo_excluir is not None and codigo_excluir not in self.estoque.set_codigos:
+            st.info(f"Nenhum produto encontrado com o código {int(codigo_excluir)}")
+            st.session_state.codigo_excluir=None
+        elif codigo_excluir is None:
+            st.info("Digite um código e clique em buscar!")
+        return
+    def renderizar_home(self):
+        st.title("Visão Geral do Estoque")
+        st.info({self.estoque.JSON_PATH})
+        try:
+            st.dataframe(
+                self.estoque.lista_dict_produtos,
+                use_container_width=True,
+                hide_index=True
+            )
+        except Exception as e:
+            print(f"Erro! {e}")
+        
+            
     def rodar(self):
         opcao=self.renderizar_menu_lateral()
         if opcao=="Cadastrar Produto":
             self.renderizar_cadastro()
         elif opcao=="Home(Listagem)":
-            st.title("Visão Geral do Estoque")
+            self.renderizar_home()
         elif opcao=="Atualizar Produto":
             self.renderizar_atualizar()
         elif opcao=="Buscar Produto":
             self.renderizar_buscar()
+        elif opcao=="Excluir Produto":
+            self.renderizar_excluir()
         else:
             st.info("Ainda em construção...")
         
